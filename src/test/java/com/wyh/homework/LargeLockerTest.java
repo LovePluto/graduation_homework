fc917232b2681c7f30d3199214cf3dee869479fd
yuheng.wang <yuheng.wang@thoughtworks.com>
2020-07-04 23:43:43 +0800

    add middle locker and finish pick up bag

--- src/test/java/com/wyh/homework/LargeLockerTest.java
+++ src/test/java/com/wyh/homework/LargeLockerTest.java
@@ -3,6 +3,7 @@ package com.wyh.homework;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 public class LargeLockerTest {
 
@@ -13,5 +14,17 @@ public class LargeLockerTest {
         Ticket ticket = largeLocker.save(new Bag());
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_get_bag_when_pick_up_given_large_locker_and_ticket() {
+        LargeLocker largeLocker = new LargeLocker(5);
+        Bag exceptedBag = new Bag();
+
+        Ticket ticket = largeLocker.save(exceptedBag);
+        Bag actualBag = largeLocker.pickUp(ticket);
+
+        assertNotNull(ticket);
+        assertSame(exceptedBag,actualBag);
     }
 }
